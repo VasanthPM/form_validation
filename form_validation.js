7e3352ae291c1cb04b0eff68280073a493b4791a
Vasantha Kumar <vasanthsrmp@gmail.com>
2022-12-26 15:49:52 +0530

    test

---
+++ form_validation.js
@@ -0,0 +1,49 @@
+class FormValidation{
+    formValues = {
+        username : "",
+        email : "",
+        phonenumber : "",
+        password : "",
+        confirmpassword : ""
+    }
+    errorValues = {
+        usernameErr : "",
+        emailErr : "",
+        phonenumberErr : "",
+        passwordErr : "",
+        confirmpasswordErr : ""
+    }
+    showErrorMsg(index,msg){
+         
+    }
+    showSuccessMsg(index){
+      
+    }
+    getInputs(){
+        
+    }
+    validateUsername(){
+
+    }
+
+    validateEmail(){
+     
+    }
+    validatePhonenumber(){
+       
+       
+    }
+    validatePassword(){
+       
+    }
+    validateConfirmpassword(){
+     
+    }
+    alertMessage(){
+       
+    }
+
+    removeInputs(){
+        
+    }
+}
